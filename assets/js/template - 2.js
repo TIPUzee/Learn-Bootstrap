bf8b54ccf9ea047ff9ad8bf54fdff2e08f0c8e63
xsTIPU <zshann992@gmail.com>
2022-07-25 04:44:21 -0700

    Going to Linux

---
+++ assets/js/template - 2.js
@@ -0,0 +1,18 @@
+
+let sidenav_menubar_opened = false;
+
+function sidenav_menu_toggle() {
+	document.querySelector('#sidenav #menubar').classList.toggle('show');
+	let menubar = document.querySelectorAll('#sidenav #menubar .doc-nav-item');
+	if (sidenav_menubar_opened == false) {
+		for (let i = 0; i < menubar.length; i++) {
+			menubar[i].style='transform: translateX(0); opacity: 1';
+		}
+		sidenav_menubar_opened = true;
+	} else {
+		for (let i = 0; i < menubar.length; i++) {
+			menubar[i].style='transform: translateX(-20px); opacity: 0';
+		}
+		sidenav_menubar_opened = false;
+	}
+}
